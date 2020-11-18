31016937bc45fb9513dae4fa124f96148b373f4f
madhu <madhu@LAPTOP-2FTNVRRG>
2020-11-17 16:37:17 -0800

    Frames topic added

--- src/main/java/WebDriverBasicConcepts/framecount.java
+++ src/main/java/WebDriverBasicConcepts/framecount.java
@@ -1,13 +1,15 @@
 package WebDriverBasicConcepts;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class frames
+public class framecount 
 {
 	@Test
 	public void frame() 
@@ -18,7 +20,10 @@ public class frames
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(10,TimeUnit.SECONDS);
 		driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_links_w3schools");
-		driver.switchTo().frame("iframeResult");
-		driver.findElement(By.xpath("//a[text()='Visit W3Schools.com!']")).click();
+        List<WebElement> totalframes=driver.findElements(By.tagName("iframe"));
+        System.out.println("total frames are-->"+totalframes.size());
 	}
 }
+	
+
+
